cfbf02af06f8217100e88e2e2f5fc9acb92f6668
AnoushkaPal924 <64146264+AnoushkaPal924@users.noreply.github.com>
2020-11-29 23:35:48 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -6,16 +6,19 @@ class Paper{
              'density':1.0,
             isStatic : false
         }
-        this.body = Bodies.circle(x, y, radius, options);
+        this.body = Bodies.circle(x, y, radius,options);
         //this.body = ellipse(x, y, radius, options);
+        this.image = loadImage("paper.png")
           this.radius = radius
         World.add(world, this.body);
       }
       display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-        ellipseMode(CENTER);
-        ellipse(0, 0, this.radius);
+       // ellipseMode(CENTER);
+        //ellipse(0, 0, this.radius);
+        imageMode(CENTER);
+        image(this.image,0,0,this.radius,this.radius);
         pop();
       }
 }
